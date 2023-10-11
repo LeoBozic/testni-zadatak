e32aa9a0e98cc23f935237cabcb057155c984510
Leo Božić <leobozic2903@gmail.com>
2023-10-11 17:14:45 +0200

    Implementing React Routes and creating pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Link, Route, Routes } from "react-router-dom";
+import { Home, Edit, Create } from "./pages/index";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/create">Create</Link>
+        <Link to="/edit">Edit</Link>
+      </nav>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/create" Component={Create} />
+        <Route path="/edit" Component={Edit} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
